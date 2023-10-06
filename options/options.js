e03ac60c4a29c640ffe294ba7ea796a338cdba8d
Crowdbotics <team@crowdbotics.com>
2023-10-06 01:29:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-pine-43786",
+  url: "https://falling-pine-43786.botics.co",
+  api: "https://falling-pine-43786.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
